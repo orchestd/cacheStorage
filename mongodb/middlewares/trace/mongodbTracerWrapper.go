87246e131987f8c71098d74ec1d64efe31bd6d86
Stav Gayer <stav.g@heilasystems.com>
2021-04-14 11:47:26 +0300

    better style wrapper calls - mongo trace middleware

--- mongodb/middlewares/trace/mongodbTracerWrapper.go
+++ mongodb/middlewares/trace/mongodbTracerWrapper.go
@@ -37,7 +37,7 @@ func NewMongoCacheStorageGetterWrapper(tracer opentracing.Tracer, conf CacheWrap
 	}
 }
 
-func getSpanFromContext(c context.Context, operationName string, tracer opentracing.Tracer, conf CacheWrapperConfiguration, tags CacheTags, f func(con context.Context) CacheStorageError) {
+func runMongoFuncWithTrace(c context.Context, operationName string, tracer opentracing.Tracer, conf CacheWrapperConfiguration, tags CacheTags, funcToRun func(con context.Context) CacheStorageError) CacheStorageError {
 	sp, con := opentracing.StartSpanFromContextWithTracer(c, tracer, operationName)
 	defer sp.Finish()
 	ext.DBType.Set(sp, cacheDbType)
@@ -63,44 +63,45 @@ func getSpanFromContext(c context.Context, operationName string, tracer opentrac
 	if tags.items != nil {
 		sp.SetTag("items", tags.items)
 	}
-	err := f(con)
-	if err != nil {
+	if err := funcToRun(con); err != nil {
 		ext.LogError(sp, err)
+		return err
 	}
+	return nil
 }
 
 func (m mongoCacheStorageGetterWrapper) GetLatestVersions(c context.Context) ([]CacheVersion, CacheStorageError) {
 	var result []CacheVersion
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		result, err = m.cacheStorageGetter.GetLatestVersions(con)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/GetLatestVersions", m.tracer, m.conf, CacheTags{}, f)
+	err := runMongoFuncWithTrace(c, "mongodb.driver/GetLatestVersions", m.tracer, m.conf, CacheTags{}, f)
 	return result, err
 }
 
 func (m mongoCacheStorageGetterWrapper) GetById(c context.Context, collectionName string, id string, ver string, dest interface{}) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageGetter.GetById(con, collectionName, id, ver, dest)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/GetById", m.tracer, m.conf, CacheTags{
+
+	err := runMongoFuncWithTrace(c, "mongodb.driver/GetById", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 		id:         &id,
 	}, f)
+
 	return err
 }
 
 func (m mongoCacheStorageGetterWrapper) GetManyByIds(c context.Context, collectionName string, ids []string, ver string, dest interface{}) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageGetter.GetManyByIds(con, collectionName, ids, ver, dest)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/GetManyByIds", m.tracer, m.conf, CacheTags{
+
+	err := runMongoFuncWithTrace(c, "mongodb.driver/GetManyByIds", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 		ids:        &ids,
@@ -110,12 +111,12 @@ func (m mongoCacheStorageGetterWrapper) GetManyByIds(c context.Context, collecti
 }
 
 func (m mongoCacheStorageGetterWrapper) GetAll(c context.Context, collectionName string, ver string, dest interface{}) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageGetter.GetAll(con, collectionName, ver, dest)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/GetById", m.tracer, m.conf, CacheTags{
+
+	err := runMongoFuncWithTrace(c, "mongodb.driver/GetById", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 	}, f)
@@ -124,12 +125,12 @@ func (m mongoCacheStorageGetterWrapper) GetAll(c context.Context, collectionName
 
 func (m mongoCacheStorageGetterWrapper) GetLatestCollectionVersion(c context.Context, collection string) (CacheVersion, CacheStorageError) {
 	var result CacheVersion
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		result, err = m.cacheStorageGetter.GetLatestCollectionVersion(con, collection)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/GetLatestCollectionVersion", m.tracer, m.conf, CacheTags{
+
+	err := runMongoFuncWithTrace(c, "mongodb.driver/GetLatestCollectionVersion", m.tracer, m.conf, CacheTags{
 		collection: &collection,
 	}, f)
 	return result, err
@@ -149,27 +150,27 @@ func NewMongoCacheStorageSetterWrapper(tracer opentracing.Tracer, conf CacheWrap
 }
 
 func (m mongoCacheStorageSetterWrapper) Insert(c context.Context, collectionName string, id string, ver string, item interface{}) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageSetter.Insert(con, collectionName, id, ver, item)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/Insert", m.tracer, m.conf, CacheTags{
+
+	err := runMongoFuncWithTrace(c, "mongodb.driver/Insert", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 		id:         &id,
 		item:       item,
 	}, f)
+
 	return err
 }
 
 func (m mongoCacheStorageSetterWrapper) InsertMany(c context.Context, collectionName string, ver string, items map[string]interface{}) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageSetter.InsertMany(con, collectionName, ver, items)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/InsertMany", m.tracer, m.conf, CacheTags{
+	err := runMongoFuncWithTrace(c, "mongodb.driver/InsertMany", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 		items:      &items,
@@ -178,12 +179,11 @@ func (m mongoCacheStorageSetterWrapper) InsertMany(c context.Context, collection
 }
 
 func (m mongoCacheStorageSetterWrapper) InsertOrUpdate(c context.Context, collectionName string, id string, ver string, item interface{}) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageSetter.InsertOrUpdate(con, collectionName, id, ver, item)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/InsertOrUpdate", m.tracer, m.conf, CacheTags{
+	err := runMongoFuncWithTrace(c, "mongodb.driver/InsertOrUpdate", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 		id:         &id,
@@ -193,12 +193,11 @@ func (m mongoCacheStorageSetterWrapper) InsertOrUpdate(c context.Context, collec
 }
 
 func (m mongoCacheStorageSetterWrapper) Update(c context.Context, collectionName string, id string, ver string, item interface{}) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageSetter.Update(con, collectionName, id, ver, item)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/Update", m.tracer, m.conf, CacheTags{
+	err := runMongoFuncWithTrace(c, "mongodb.driver/Update", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 		id:         &id,
@@ -208,12 +207,11 @@ func (m mongoCacheStorageSetterWrapper) Update(c context.Context, collectionName
 }
 
 func (m mongoCacheStorageSetterWrapper) Remove(c context.Context, collectionName string, id string, ver string) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageSetter.Remove(con, collectionName, id, ver)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/Remove", m.tracer, m.conf, CacheTags{
+	err := runMongoFuncWithTrace(c, "mongodb.driver/Remove", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 		id:         &id,
@@ -222,12 +220,11 @@ func (m mongoCacheStorageSetterWrapper) Remove(c context.Context, collectionName
 }
 
 func (m mongoCacheStorageSetterWrapper) RemoveAll(c context.Context, collectionName string, ver string) CacheStorageError {
-	var err CacheStorageError
-	f := func(con context.Context) CacheStorageError {
+	f := func(con context.Context) (err CacheStorageError) {
 		err = m.cacheStorageSetter.RemoveAll(con, collectionName, ver)
 		return err
 	}
-	getSpanFromContext(c, "mongodb.driver/RemoveAll", m.tracer, m.conf, CacheTags{
+	err := runMongoFuncWithTrace(c, "mongodb.driver/RemoveAll", m.tracer, m.conf, CacheTags{
 		collection: &collectionName,
 		ver:        &ver,
 	}, f)
